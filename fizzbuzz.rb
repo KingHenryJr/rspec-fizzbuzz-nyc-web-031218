d99ee5b1418dd40b680ebc0838b3b989e47ac7ef
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-21 17:51:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 def fizzbuzz(num)
   
-  if num % 3 === 0 
+  if num % 5 === 0 && num % 3 === 0
+    return "FizzBuzz"
+  
+  elsif num % 3 === 0 
     return "Fizz"
 
   elsif num % 5 === 0
     return "Buzz"
     
-  elsif num % 5 === 0 && num % 3 === 0
-    return "FizzBuzz"
     
   else return nil
 
